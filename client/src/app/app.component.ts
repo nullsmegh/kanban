16259b157dabe3bb24f7174b7b4a600a993e83da
Your Name <you@example.com>
2019-06-17 17:02:43 +0530

    bug fixes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,19 +9,98 @@ import {MatSnackBar} from '@angular/material';
 })
 export class AppComponent {
 
-  title = 'client';
-  stages = ["planning", "ongoning", "done"];
-  project1 = ["pizza" , "poop", "shit", "duck"];
-  project2 = ["kik" , "lol", "lulu", "duk"];
+  stages = [{
+    title: "Planning",
+    data: [{
+      task: "Task 1 - Planning",
+      metaData: "Low Priority"
+    },{
+      task: "Task 2 - Planning",
+      metaData: "Low Priority"
+    },{
+      task: "Task 3 - Planning",
+      metaData: "Low Priority"
+    },{
+      task: "Task 4 - Planning",
+      metaData: "Low Priority"
+    }]
+  }, {
+    title: "Ongoing",
+    data: [{
+      task: "Task 1 - Ongoing",
+      metaData: "Low Priority"
+    },{
+      task: "Task 2 - Ongoing",
+      metaData: "Low Priority"
+    },{
+      task: "Task 3 - Ongoing",
+      metaData: "Low Priority"
+    },{
+      task: "Task 4 - Ongoing",
+      metaData: "Low Priority"
+    }]
+  }, {
+    title: "Completed",
+    data: [{
+      task: "Task 1 - Completed",
+      metaData: "Low Priority"
+    },{
+      task: "Task 2 - Completed",
+      metaData: "Low Priority"
+    },{
+      task: "Task 3 - Completed",
+      metaData: "Low Priority"
+    },{
+      task: "Task 4 - Completed",
+      metaData: "Low Priority"
+    }]
+  }];
+
+  taskTitle: string = "";
+  taskMetaData: string = "";
+  stageTitle: string = "";
 
   constructor(public snackBar: MatSnackBar){
 
   }
 
+  addJob(index,title,meta){
+    if(!title.innerHTML) {
+      this.emptyFieldError();
+    } else {
+      this.stages[index].data.unshift({task: title.innerHTML, metaData: meta.innerHTML || "Tag"});
+      this.openSnackBar();
+    }
+
+    title.innerHTML = "";
+    meta.innerHTML  = "";
+
+    return;
+  }
+
+  addStage(title){
+    if(!title){
+      this.emptyFieldError();
+    } else {
+      this.stages.push({
+      title: title.innerHTML,
+      data: []
+      });
+    }
+    title.innerHTML = "";
+    return;
+  }
+
   openSnackBar() {
-    this.snackBar.open("sasadad", "sadsad", {
+    this.snackBar.open("Added", "OK", {
       duration: 2000,
     });
+  }
+
+  emptyFieldError(){
+    this.snackBar.open("Please fill required fields.", "Error", {
+      duration: 2000,
+    });    
   }
 
   drop(event: CdkDragDrop<string[]>) {
